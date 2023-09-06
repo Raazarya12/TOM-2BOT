6cddaf20d6dc595f99ba708cef8e9f02580dc008
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-09-06 14:44:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CyberTG/TOM-BOT.git /TOM-BOT
+  git clone https://github.com/Jkdeveloper01/TOM-2BOT.git /TOM-2BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TOM-BOT
+  git clone $UPSTREAM_REPO /TOM-2BOT
 fi
-cd /TOM-BOT
+cd /TOM-2BOT
 pip3 install -U -r requirements.txt
 echo "Starting...."
 python3 bot.py
